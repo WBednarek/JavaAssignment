b48f320c3f24f0bb92e07f90b0ebb6ac70182c6c
Wiktor Bednarek <200190@student.pwr.edu.pl>
2017-04-05 00:34:17 +0200

    First commit JavaFX

---
+++ src/sample/Controller.java
@@ -0,0 +1,10 @@
+package sample;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void pressButon(ActionEvent event)
+    {
+        System.out.println("Java assignment");
+    }
+}
